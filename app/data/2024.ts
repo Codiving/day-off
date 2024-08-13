1b81f8159271d75bd90caeb68eaa8039e02b14e4
Codiving <codivingmoon@gmail.com>
2024-08-13 22:53:08 +0900

    chore: 2024년 연휴 데이터 추가

---
+++ app/data/2024.ts
@@ -0,0 +1,78 @@
+export const HOLIDAY_2024 = [
+  {
+    date: 20240101,
+    description: "신정"
+  },
+  {
+    date: 20240209,
+    description: "설"
+  },
+  {
+    date: 20240210,
+    description: "설"
+  },
+  {
+    date: 20240211,
+    description: "설"
+  },
+  {
+    date: 20240212,
+    description: "대체공휴일"
+  },
+  {
+    date: 20240301,
+    description: "삼일절"
+  },
+  {
+    date: 20240410,
+    description: "국회의원 선거"
+  },
+  {
+    date: 20240501,
+    description: "근로자의 날"
+  },
+  {
+    date: 20240505,
+    description: "어린이 날"
+  },
+  {
+    date: 20240506,
+    description: "대체공휴일"
+  },
+  {
+    date: 20240515,
+    description: "부처님 오신 날"
+  },
+  {
+    date: 20240606,
+    description: "현충일"
+  },
+  {
+    date: 20240815,
+    description: "광복절"
+  },
+  {
+    date: 20240916,
+    description: "추석"
+  },
+  {
+    date: 20240917,
+    description: "추석"
+  },
+  {
+    date: 20240918,
+    description: "추석"
+  },
+  {
+    date: 20241003,
+    description: "개천절"
+  },
+  {
+    date: 20241009,
+    description: "한글날"
+  },
+  {
+    date: 20241225,
+    description: "크리스마스"
+  }
+];
