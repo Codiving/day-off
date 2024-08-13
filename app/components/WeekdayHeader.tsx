2642332db855fbc38e27f1bf5ede3d0d4e18e55c
Codiving <codivingmoon@gmail.com>
2024-08-13 19:46:22 +0900

    feat: WeekdayHeader 컴포넌트 추가
    일요일-토요일을 나타내는 컴포넌트

---
+++ app/components/WeekdayHeader.tsx
@@ -0,0 +1,27 @@
+export default function WeekdayHeader() {
+  return (
+    <>
+      <div className="text-center p-3">
+        <span className="uppercase font-bold text-xl text-red-500">Sun</span>
+      </div>
+      <div className="text-center p-3">
+        <span className="uppercase font-bold text-xl">Mon</span>
+      </div>
+      <div className="text-center p-3">
+        <span className="uppercase font-bold text-xl">Tue</span>
+      </div>
+      <div className="text-center p-3">
+        <span className="uppercase font-bold text-xl">Wed</span>
+      </div>
+      <div className="text-center p-3">
+        <span className="uppercase font-bold text-xl">Thu</span>
+      </div>
+      <div className="text-center p-3">
+        <span className="uppercase font-bold text-xl">Fri</span>
+      </div>
+      <div className="text-center p-3">
+        <span className="uppercase font-bold text-xl text-blue-500">Sat</span>
+      </div>
+    </>
+  );
+}
