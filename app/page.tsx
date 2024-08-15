8a88e528e65839a3d610276479b3321eff1ad6bd
Codiving <codivingmoon@gmail.com>
2024-08-15 20:45:06 +0900

    feat: 스크롤 시 block별 이동하도록 기능 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,39 +1,66 @@
 "use client";
 import dayjs from "dayjs";
-import { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Calendar from "./components/Calendar";
 import Header from "./components/Header";
 
 export default function Home() {
   const [currentDate, setCurrentDate] = useState(dayjs());
+  const containerRef = useRef<HTMLDivElement | null>(null);
 
   const changeMonth = (offset: number) => {
     const newDate = currentDate.add(offset, "month");
     setCurrentDate(newDate);
   };
 
-  console.log("currentDate.month() : ", currentDate.month());
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    const sections = container.querySelectorAll<HTMLDivElement>(".item");
+
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting) {
+            entry.target.scrollIntoView({
+              behavior: "smooth",
+              block: "center"
+            });
+          }
+        });
+      },
+      {
+        root: container,
+        rootMargin: "0px",
+        threshold: 0.5
+      }
+    );
+
+    sections.forEach(section => observer.observe(section));
+
+    return () => {
+      sections.forEach(section => observer.unobserve(section));
+    };
+  }, []);
 
   return (
-    <>
-      <Header />
-      <main className="bg-gray-100">
-        <section className="flex gap-4 max-w-6xl mx-auto px-4 py-8">
-          {/* <div className="flex-1">
-            <p>{MONTH_DESCRIPTION[currentDate.month()].description}</p>
-          </div> */}
-          <div className="flex-1 flex justify-between items-center mb-4">
-            <h2 className="pl-8 text-3xl font-bold mb-7">
-              {currentDate.format("YYYY년 MM월")}
-            </h2>
-          </div>
+    <div id="scroll-container" ref={containerRef}>
+      <main className="relative item flex flex-col w-full">
+        <div className="absolute top-0 w-full">
+          <Header />
+        </div>
+        <section className="w-full flex gap-4">
           <Calendar
-            className="w-3/4"
+            className="w-full mx-auto"
             year={currentDate.year()}
             month={currentDate.month() + 1}
           />
         </section>
       </main>
-    </>
+      <section className="item bg-slate-300">1</section>
+      <section className="item bg-red-300">2</section>
+      <section className="item bg-yellow-300">3</section>
+    </div>
   );
 }
