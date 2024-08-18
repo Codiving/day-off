2b57e5cca303b4b05648fd6746c63b7e32d2a630
Codiving <codivingmoon@gmail.com>
2024-08-18 22:12:46 +0900

    feat: 오늘 날짜로 이동하는 기능 추가

--- app/components/HolidayCalendar/Calendar/index.tsx
+++ app/components/HolidayCalendar/Calendar/index.tsx
@@ -5,7 +5,7 @@ import OtherMonthDay from "./OtherMonthDay";
 import useDateStore from "@/app/store/useDateStore";
 
 export default function Calendar() {
-  const { date } = useDateStore();
+  const { date, setDate } = useDateStore();
   const year = date.year();
   const month = date.month() + 1;
 
@@ -39,55 +39,65 @@ export default function Calendar() {
   });
 
   return (
-    <div id="calendar" className="flex-1 grid grid-cols-7 gap-2 text-center">
-      <WeekdayHeader />
-      {prev.map(day => {
-        return <OtherMonthDay key={`prev-${day}`} day={day} />;
-      })}
-      {current.map(day => {
-        const dayDate = currentDate.set("date", day);
-        const dayOfWeek = dayDate.day();
-        const isSunday = dayOfWeek === 0;
-        const isSaturday = dayOfWeek === 6;
+    <div className="flex flex-col flex-1">
+      <button
+        onClick={() => {
+          setDate(dayjs());
+        }}
+        className="text-right text-xl pr-9"
+      >
+        오늘
+      </button>
+      <div id="calendar" className="flex-1 grid grid-cols-7 gap-2 text-center">
+        <WeekdayHeader />
+        {prev.map(day => {
+          return <OtherMonthDay key={`prev-${day}`} day={day} />;
+        })}
+        {current.map(day => {
+          const dayDate = currentDate.set("date", day);
+          const dayOfWeek = dayDate.day();
+          const isSunday = dayOfWeek === 0;
+          const isSaturday = dayOfWeek === 6;
 
-        const dateNumber = Number(
-          `${year}${month.toString().padStart(2, "0")}${String(day).padStart(
-            2,
-            "0"
-          )}`
-        );
-        const holiday = dateInfo.holiday.find(
-          ({ date }) => Number(dayjs(date).format("YYYYMMDD")) === dateNumber
-        );
+          const dateNumber = Number(
+            `${year}${month.toString().padStart(2, "0")}${String(day).padStart(
+              2,
+              "0"
+            )}`
+          );
+          const holiday = dateInfo.holiday.find(
+            ({ date }) => Number(dayjs(date).format("YYYYMMDD")) === dateNumber
+          );
 
-        return (
-          <div
-            key={`current-${day}`}
-            className="justify-center items-center relative p-4 text-2xl flex flex-col gap-2"
-          >
-            <span
-              className={`${
-                holiday ? "rounded-full bg-red-500 text-white py-1 px-2" : ""
-              } ${
-                isSunday ? "text-red-500" : isSaturday ? "text-blue-500" : ""
-              }`}
+          return (
+            <div
+              key={`current-${day}`}
+              className="justify-center items-center relative p-4 text-2xl flex flex-col gap-2"
             >
-              {String(day).padStart(2, "0")}
-            </span>
-
-            {!!holiday && (
               <span
-                className={`bottom-0 absolute text-sm left-1/2 transform -translate-x-1/2 whitespace-nowrap`}
+                className={`${
+                  holiday ? "rounded-full bg-red-500 text-white py-1 px-2" : ""
+                } ${
+                  isSunday ? "text-red-500" : isSaturday ? "text-blue-500" : ""
+                }`}
               >
-                {holiday.name}
+                {String(day).padStart(2, "0")}
               </span>
-            )}
-          </div>
-        );
-      })}
-      {next.map(day => {
-        return <OtherMonthDay key={`next-${day}`} day={day} />;
-      })}
+
+              {!!holiday && (
+                <span
+                  className={`bottom-0 absolute text-sm left-1/2 transform -translate-x-1/2 whitespace-nowrap`}
+                >
+                  {holiday.name}
+                </span>
+              )}
+            </div>
+          );
+        })}
+        {next.map(day => {
+          return <OtherMonthDay key={`next-${day}`} day={day} />;
+        })}
+      </div>
     </div>
   );
 }
