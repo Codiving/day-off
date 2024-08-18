57c7d5eb0bec688530a482b06d7db39325ef804a
Codiving <codivingmoon@gmail.com>
2024-08-18 23:57:57 +0900

    chore: 필요없는 텍스트 제거

--- app/components/HolidayCalendar/ClosestHoliday/HolidayInfo.tsx
+++ app/components/HolidayCalendar/ClosestHoliday/HolidayInfo.tsx
@@ -12,11 +12,10 @@ export default function HolidayInfo(props: HolidayInfoProps) {
 
   return (
     <>
-      <p className="text-xl">가까운 공휴일</p>
+      <p className="text-xl">공휴일</p>
       <p className="text-3xl">
         {dayjs(date).format("MM월 DD일")} - {name}
       </p>
-      <p className="text-xl text-slate-500">{description}</p>
       <div className="w-72 h-72 rounded-full overflow-hidden flex items-center justify-center relative">
         <Image src="/koreanFlag.jpg" alt="태극기" fill sizes="auto" priority />
       </div>
--- app/components/Intro/index.tsx
+++ app/components/Intro/index.tsx
@@ -7,19 +7,10 @@ dayjs.extend(duration);
 export default function Intro() {
   const holiday = getClosestHoliday();
   return (
-    <div className="relative flex flex-col gap-20 items-center">
+    <div>
       <header>
         <h1 className="text-5xl">연차쓰기 좋은 날!</h1>
       </header>
-      <div className="text-center flex flex-col gap-8">
-        <div className="flex flex-col text-center">
-          <p>
-            힘든 일상을 보내는 직장인들을 위해, 연차를 잘 활용할 수 있는 날들을
-            추천드립니다.
-          </p>
-          <p>효율적으로 연차를 계획하여 행복한 휴가를 즐기시길 바랍니다.</p>
-        </div>
-      </div>
       {!holiday && <p>이번년도 남은 공휴일이 없습니다.</p>}
       {holiday && <ClosestHolidayCountDown holiday={holiday} />}
     </div>
