a7dd987bae3a9292311d1756efcb635f65824cf4
Codiving <codivingmoon@gmail.com>
2024-08-19 22:23:45 +0900

    fix: 꿀 연차 모아보기 년도 수정

--- app/components/DayOffAbbrev/index.tsx
+++ app/components/DayOffAbbrev/index.tsx
@@ -7,12 +7,12 @@ export default function DayOffAbbrev() {
 
   return (
     <div className="max-w-4xl h-full mx-auto flex flex-col gap-16 justify-center items-center">
-      <h3 className="text-2xl sm:text-4xl">2024년 꿀 연차 모아보기</h3>
+      <h3 className="text-2xl sm:text-4xl">{date.year()}년 꿀 연차 모아보기</h3>
       <div className="flex flex-col gap-6 sm:gap-12">
         {Object.entries(description).map(([month, description]) => {
           return description.map((list, index) => {
             return (
-              <div key={index} className="flex flex-col gap-4">
+              <div key={index} className="flex flex-col gap-4 text-center">
                 {list.map((desc, index) => {
                   if (index === 0) {
                     return (
@@ -25,7 +25,7 @@ export default function DayOffAbbrev() {
                     );
                   }
                   return (
-                    <p className="text-sm sm:text-xl text-center" key={desc}>
+                    <p className="text-sm sm:text-xl" key={desc}>
                       * {desc} 연차 추천
                     </p>
                   );
