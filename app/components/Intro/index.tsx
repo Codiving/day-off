deaba7aea776581f56db0496f35c018ab9ccac22
Codiving <codivingmoon@gmail.com>
2024-08-19 19:01:23 +0900

    feat: Header 사이트 설명 추가

--- app/components/Intro/index.tsx
+++ app/components/Intro/index.tsx
@@ -7,10 +7,13 @@ dayjs.extend(duration);
 export default function Intro() {
   const holiday = getClosestHoliday();
   return (
-    <div>
-      <header>
-        <h1 className="text-5xl">연차쓰기 좋은 날!</h1>
-      </header>
+    <div className="relative px-4 h-full">
+      <div className="py-8">
+        <header>
+          <h1 className="text-5xl">연차쓰기 좋은 날!</h1>
+        </header>
+        <p className="mt-4">연차쓰기 꿀인 날짜를 가르쳐드립니다.</p>
+      </div>
       {!holiday && <p>이번년도 남은 공휴일이 없습니다.</p>}
       {holiday && <ClosestHolidayCountDown holiday={holiday} />}
     </div>
