81f17b3bbf9e01f1c2507d9b21eeba6596481c14
Codiving <codivingmoon@gmail.com>
2024-08-19 18:59:03 +0900

    chore: 2024년 연차 추천일 설명글 추가

--- app/data/date.ts
+++ app/data/date.ts
@@ -1,16 +1,23 @@
 import { DAY_OFF_2024 } from "./2024/dayOff";
+import { DESCRIPTION_2024 } from "./2024/description";
 import { HOLIDAY_2024 } from "./2024/holiday";
+
+export type Description = {
+  [key in number]: string[][];
+};
 
 type IDate = {
   [key in number]: {
     holiday: Holiday[];
     dayOff: number[];
+    description: Description;
   };
 };
 
 export const DATE: IDate = {
   2024: {
     holiday: HOLIDAY_2024,
-    dayOff: DAY_OFF_2024
+    dayOff: DAY_OFF_2024,
+    description: DESCRIPTION_2024
   }
 };
