cc1b81a45aea9bbda8a7c452fa806aa2da92884a
Codiving <codivingmoon@gmail.com>
2024-08-19 22:24:25 +0900

    style: 연차 추천 이유 css 수정

--- app/components/HolidayCalendar/RecommandDesc.tsx
+++ app/components/HolidayCalendar/RecommandDesc.tsx
@@ -5,24 +5,28 @@ interface RecommandDescProps {
 export default function RecommandDesc(props: RecommandDescProps) {
   const { description } = props;
   return (
-    <div className="px-4 flex flex-col gap-2">
-      {description.map(list =>
-        list.map((desc, index) => {
-          if (index === 0) {
-            return (
-              <p className={"text-sm sm:text-xl"} key={desc}>
-                {desc}
-              </p>
-            );
-          }
+    <div className="px-4 flex flex-col gap-4 sm:gap-10">
+      {description.map((list, index) => {
+        return (
+          <div key={index} className="flex flex-col gap-1">
+            {list.map((desc, index) => {
+              if (index === 0) {
+                return (
+                  <p className={"text-sm sm:text-xl"} key={desc}>
+                    {desc}
+                  </p>
+                );
+              }
 
-          return (
-            <p key={desc} className="text-2xl sm:text-4xl">
-              <span className="text-green-800">{desc}</span> 연차 추천
-            </p>
-          );
-        })
-      )}
+              return (
+                <p key={desc} className="text-2xl sm:text-4xl">
+                  <span className="text-green-800">{desc}</span> 연차 추천
+                </p>
+              );
+            })}
+          </div>
+        );
+      })}
     </div>
   );
 }
