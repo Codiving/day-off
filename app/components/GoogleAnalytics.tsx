14ed32930f12dfe9b46c21815490d99d876fb7bb
Codiving <codivingmoon@gmail.com>
2024-08-20 18:58:07 +0900

    feat: GoogleAnalytics 컴포넌트 추가

---
+++ app/components/GoogleAnalytics.tsx
@@ -0,0 +1,24 @@
+import Script from "next/script";
+
+export default function GoogleAnalytics() {
+  return (
+    <>
+      <Script
+        async
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA}`}
+      />
+      <Script
+        id="google-analytics"
+        dangerouslySetInnerHTML={{
+          __html: `
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', '${process.env.NEXT_PUBLIC_GA}');
+		`
+        }}
+      />
+    </>
+  );
+}
